e64eec17a38df4101c77013301b3a1d62389fd16
MaksiConGit <122688647+MaksiConGit@users.noreply.github.com>
2025-05-06 13:54:39 -0300

    fix: se arregla el estilo del checkbox

--- resources/views/components/form-input-checkbox.blade.php
+++ resources/views/components/form-input-checkbox.blade.php
@@ -1,16 +1,31 @@
-<div class="col-auto">
-  <label for="">{{$label}}</label>
+<style>
+  .form-group.checkbox-group {
+    display: flex;
+    align-items: center;
+    gap: 10px;
+  }
+
+  .form-group.checkbox-group label {
+    display: inline-block;
+  }
+
+  .form-group.checkbox-group .colorinput {
+    display: inline-block;
+    vertical-align: middle;
+  }
+</style>
+
+<div class="form-group checkbox-group">
+  <label for="{{$name}}" class="mr-2">{{ $label }}</label>
   <label class="colorinput">
     <input
       name="{{$name}}"
       type="checkbox"
       id="{{$name}}"
       value="{{$value}}"
-      class="colorinput-input" 
+      class="colorinput-input"
       {{$checked}}
     />
     <span class="colorinput-color bg-{{$color}}"></span>
   </label>
 </div>
-
-{{-- <input type="checkbox" name="is_active" id="is_active" value="1" checked> --}}
