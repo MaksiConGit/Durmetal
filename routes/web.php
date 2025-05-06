27d0af49b896b853bdf2012aa0bd14a3adc7234a
MaksiConGit <122688647+MaksiConGit@users.noreply.github.com>
2025-05-06 18:36:31 -0300

    fix: el logo del layout te lleva a los hornos

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,8 @@ Route::middleware('auth')->group(function () {
 
     Route::get('/', function () {
         return view('index');
-    });
+    })->name('index');
+    
 
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
