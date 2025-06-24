d371103aa7566fbcc3715dec6fffb6b1a6086e12
MaksiConGit <122688647+MaksiConGit@users.noreply.github.com>
2025-06-24 16:49:46 -0300

    fix: codigo complejidad

---
+++ resources/views/components/form-livewire.blade.php
@@ -0,0 +1,25 @@
+{{-- <div class="container"> --}}
+    {{-- <div class="page-inner"> --}}
+      <div class="row">
+        <div class="col-md-12">
+          <form action="{{ $action }}" method="POST" {{$livewire}}>
+            @csrf
+            {{$method}}
+            <div class="card">
+              <div class="card-header">
+                <div class="card-title">{{ $card_title }}</div>
+              </div>
+              <div class="card-body">
+                <div class="row">
+                  {{ $inputs }}
+                </div>
+              </div>              
+              <div class="card-action">
+                {{ $buttons }}
+              </div>
+            </div>
+          </form>
+        </div>
+    {{-- </div> --}}
+  {{-- </div> --}}
+  
