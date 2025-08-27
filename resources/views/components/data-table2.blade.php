21eb54aa76ea2910432fd06114e3cb3b1bd9d7a9
MaksiConGit <122688647+MaksiConGit@users.noreply.github.com>
2025-08-27 14:25:55 -0300

    feat: data y simple table

---
+++ resources/views/components/data-table2.blade.php
@@ -0,0 +1,15 @@
+<table id="example2" class="table table-bordered table-hover">
+
+  <thead class="bg-dark text-white">
+    {{ $thead }}
+  </thead>
+
+  <tbody>
+    {{ $tbody }}
+  </tbody>
+
+  <tfoot class="bg-dark text-white">
+    {{ $thead }}
+  </tfoot>
+
+</table>
---
+++ resources/views/components/simple-table2.blade.php
@@ -0,0 +1,23 @@
+<!-- /.row -->
+<div class="row">
+  <div class="col-12">
+    <div class="card">
+      <div class="card-header">
+        {{ $filtros }}
+      <!-- /.card-header -->
+      <div class="card-body table-responsive p-0" style="height: 500px;">
+        <table class="table table-head-fixed text-nowrap table-hover table-bordered table-striped">
+          <thead>
+            {{ $thead }}
+          </thead>
+          <tbody>
+            {{ $tbody }}
+          </tbody>
+        </table>
+      </div>
+      <!-- /.card-body -->
+    </div>
+    <!-- /.card -->
+  </div>
+</div>
+<!-- /.row -->
