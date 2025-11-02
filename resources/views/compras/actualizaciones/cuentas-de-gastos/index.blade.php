1c1144f89ceb7a4074f87e2453a4d9b8e58dd6a5
MaksiConGit <122688647+MaksiConGit@users.noreply.github.com>
2025-11-02 16:54:16 -0300

    feat: visual cuenta de gastos y proveedores

--- resources/views/compras/actualizaciones/cuentas-de-gastos/index.blade.php
+++ resources/views/compras/actualizaciones/cuentas-de-gastos/index.blade.php
@@ -1,4 +1,4 @@
-<x-layout>
+{{-- <x-layout>
 
     <x-slot name="title">Compras</x-slot>
     <x-slot name="breadcrumbs">
@@ -73,4 +73,87 @@
         </x-slot>
     </x-data-table>
 
-</x-layout>
+</x-layout> --}}
+
+<div>
+  <x-layout2-sidebar>
+      <x-slot name="title">Cuentas de Gastos</x-slot>
+
+      <x-slot name="filtros">
+
+          <div class="form-inline mt-5 mr-3 d-flex flex-column align-items-center">
+
+              {{-- <div class="form-group mb-3">
+                  <a href="{{ route('clients.create') }}" 
+                  class="btn btn-app bg-primary">
+                      <i class="fas fa-plus"></i> Nuevo
+                  </a>
+              </div>
+
+              <div class="form-group mb-3">
+                  <a href="{{ $selectedId ? route('clients.edit', $selectedId) : '#' }}" 
+                  class="btn btn-app bg-primary {{ !$selectedId ? 'disabled' : '' }}">
+                      <i class="fas fa-pen"></i> Modificar
+                  </a>
+              </div>
+
+              <div class="form-group mb-3">
+                  <form
+                      action="{{ $selectedId ? route('clients.destroy', $selectedId) : '#' }}"
+                      method="POST"
+                      onsubmit="return confirm('¿Estás seguro de que quieres eliminar este cliente?')"
+                      class="m-0 p-0"
+                  >
+                      @csrf
+                      @method('DELETE')
+                      <button
+                          type="submit"
+                          class="btn btn-app bg-primary {{ !$selectedId ? 'disabled' : '' }}"
+                          data-bs-toggle="tooltip"
+                          title="Eliminar programación"
+                      >
+                      <i class="fas fa-trash-can"></i> Eliminar
+                      </button>
+                  </form>
+              </div> --}}
+
+          </div>
+
+      </x-slot>
+
+      <x-simple-table2>
+
+          <x-slot name="thead">
+              <tr>
+                  <th>NOMBRE</th>
+                  <th>DESCRIPCION</th>
+              </tr>
+          </x-slot>
+          <x-slot name="tbody">
+              {{-- @forelse ($clients as $client)
+                  <tr wire:click="selectClient({{ $client->id }})" 
+                      class="{{ $selectedId === $client->id ? 'table-primary' : '' }}" 
+                      style="cursor: pointer;">
+                      <td>{{ $client->id }}</td>
+                      <td>{{ $client->Nombre }}</td>
+                      <td>{{ $client->Domicilio }}</td>
+                      <td>{{ $client->localidad->Nombre ?? 'Ciudad no asignada' }}</td>
+                      <td>{{ $client->localidad->provincia->Nombre ?? 'Provincia no asignada' }}</td>
+                      <td>{{ $client->Telefono }}</td>
+                      <td>{{ $client->condicionIVA->Nombre }}</td>
+                      <td>{{ $client->TipoDocumento }}</td>
+                      <td>{{ $client->NroDocumento }}</td>
+                      <td class="text-center">
+                          <input type="checkbox" {{ $client->Activo == 1 ? 'checked' : '' }} disabled>
+                      </td>
+                  </tr>
+              @empty
+                  <tr><td colspan="11">No se encontraron resultados.</td></tr>
+              @endforelse --}}
+          </x-slot>
+
+      </x-simple-table2>
+
+  </x-layout2-sidebar>
+
+</div>
--- resources/views/compras/actualizaciones/proveedores/index.blade.php
+++ resources/views/compras/actualizaciones/proveedores/index.blade.php
@@ -1,4 +1,4 @@
-<x-layout>
+{{-- <x-layout>
 
     <x-slot name="title">Compras</x-slot>
     <x-slot name="breadcrumbs">
@@ -13,4 +13,110 @@
 
     @livewire('barra-busqueda-proveedor')
 
-</x-layout>
+</x-layout> --}}
+
+
+<div>
+  <x-layout2-sidebar>
+      <x-slot name="title">Proveedores</x-slot>
+
+      <x-slot name="filtros">
+
+          <div class="form-inline mt-5 mr-3 d-flex flex-column align-items-center">
+
+              {{-- <div class="form-group mb-3">
+                  <a href="{{ route('clients.create') }}" 
+                  class="btn btn-app bg-primary">
+                      <i class="fas fa-plus"></i> Nuevo
+                  </a>
+              </div>
+
+              <div class="form-group mb-3">
+                  <a href="{{ $selectedId ? route('clients.edit', $selectedId) : '#' }}" 
+                  class="btn btn-app bg-primary {{ !$selectedId ? 'disabled' : '' }}">
+                      <i class="fas fa-pen"></i> Modificar
+                  </a>
+              </div>
+
+              <div class="form-group mb-3">
+                  <form
+                      action="{{ $selectedId ? route('clients.destroy', $selectedId) : '#' }}"
+                      method="POST"
+                      onsubmit="return confirm('¿Estás seguro de que quieres eliminar este cliente?')"
+                      class="m-0 p-0"
+                  >
+                      @csrf
+                      @method('DELETE')
+                      <button
+                          type="submit"
+                          class="btn btn-app bg-primary {{ !$selectedId ? 'disabled' : '' }}"
+                          data-bs-toggle="tooltip"
+                          title="Eliminar programación"
+                      >
+                      <i class="fas fa-trash-can"></i> Eliminar
+                      </button>
+                  </form>
+              </div> --}}
+
+          </div>
+
+      </x-slot>
+
+      <x-simple-table2>
+
+          <x-slot name="filtros">
+
+            <div class="row">
+
+                <div class="col-4">
+                    <div class="form-group mb-0">
+                        <label for="filtro1" class="font-weight-normal">CODIGO | NOMBRE | N° DOCUMENTO</label>
+                        <input type="text" id="filtro1" name="filtro1" wire:model.live="search" class="form-control form-control-sm filtro-input" placeholder="Buscar...">
+                    </div>
+                </div>
+
+            </div>
+
+          </x-slot>
+
+          <x-slot name="thead">
+              <tr>
+                  <th>CODIGO</th>
+                  <th>NOMBRE</th>
+                  <th>DOMICILIO</th>
+                  <th>LOCALIDAD</th>
+                  <th>PROVINCIA</th>
+                  <th>TELEFONO</th>
+                  <th>CONDICION IVA</th>
+                  <th>RETENCION IIBB</th>
+                  <th>ACTIVO</th>
+              </tr>
+          </x-slot>
+          <x-slot name="tbody">
+              {{-- @forelse ($clients as $client)
+                  <tr wire:click="selectClient({{ $client->id }})" 
+                      class="{{ $selectedId === $client->id ? 'table-primary' : '' }}" 
+                      style="cursor: pointer;">
+                      <td>{{ $client->id }}</td>
+                      <td>{{ $client->Nombre }}</td>
+                      <td>{{ $client->Domicilio }}</td>
+                      <td>{{ $client->localidad->Nombre ?? 'Ciudad no asignada' }}</td>
+                      <td>{{ $client->localidad->provincia->Nombre ?? 'Provincia no asignada' }}</td>
+                      <td>{{ $client->Telefono }}</td>
+                      <td>{{ $client->condicionIVA->Nombre }}</td>
+                      <td>{{ $client->TipoDocumento }}</td>
+                      <td>{{ $client->NroDocumento }}</td>
+                      <td class="text-center">
+                          <input type="checkbox" {{ $client->Activo == 1 ? 'checked' : '' }} disabled>
+                      </td>
+                  </tr>
+              @empty
+                  <tr><td colspan="11">No se encontraron resultados.</td></tr>
+              @endforelse --}}
+          </x-slot>
+
+      </x-simple-table2>
+
+  </x-layout2-sidebar>
+
+</div>
