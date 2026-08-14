4a23d04b039cb36583cba6822f91ca59710e2c15
MaksiConGit <maksicongit@gmail.com>
2026-08-14 20:59:51 -0300

    fix

--- app/Services/AfipService.php
+++ app/Services/AfipService.php
@@ -13,7 +13,7 @@ class AfipService
     {
         $this->afip = new Afip([
             'CUIT' => env('AFIP_CUIT'),
-            'access_token' => env('AFIP_ACCESS_TOKEN'),
+            'access_token' => '6u8I7tcgUzD1YQZ65O55VVgLZaP4V8caATPiXuEBZZzjTBaukOio1oW7SBX3g3l1',
         ]);
     }
 
