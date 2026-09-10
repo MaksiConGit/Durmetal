320c3a7bb1d0850389975776164e45dca08af6c5
MaksiConGit <maksicongit@gmail.com>
2026-09-10 13:57:25 -0300

    fix: arca .crt y .key get_contents

--- app/Services/AfipService.php
+++ app/Services/AfipService.php
@@ -14,8 +14,8 @@ class AfipService
         $this->afip = new Afip([
             'CUIT' => env('AFIP_CUIT'),
             'access_token' => 'PHhLSxkgIAXnzew8AyHTLAiVbTCyofWcXnxuhJGPmUdFg51MyHDa00A1UJbQb9eQ',
-            'cert' => storage_path('app/afip/certificado.crt'),
-            'key' => storage_path('app/afip/clave.key'),
+            'cert' => file_get_contents(storage_path('app/afip/certificado.crt')),
+            'key' => file_get_contents(storage_path('app/afip/clave.key')),
             'production' => true,
         ]);
     }
